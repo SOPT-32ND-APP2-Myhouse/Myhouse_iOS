e82b3a6b8e2288e015fe4407287ca12ec1b28244
frohsch <kminjoo1213@gmail.com>
2023-05-13 16:22:20 +0900

    [ADD] 프로젝트 생성

---
+++ Myhouse/Myhouse/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Myhouse
+//
+//  Created by Minjoo Kim on 2023/05/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
