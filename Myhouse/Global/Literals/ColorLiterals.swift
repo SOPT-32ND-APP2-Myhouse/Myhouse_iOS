6bbd8173060112fa2940ca3b89059456775c65e9
Choi YoungLyn <joeuni0129@gmail.com>
2023-05-14 03:53:25 +0900

    [Setting] #5 - ColorLiterals 추가

---
+++ Myhouse/Global/Literals/ColorLiterals.swift
@@ -0,0 +1,44 @@
+//
+//  ColorLiterals.swift
+//  Myhouse
+//
+//  Created by 최영린 on 2023/05/14.
+//
+
+import UIKit
+
+extension UIColor {
+
+    @nonobjc class var main: UIColor {
+        return UIColor(red: 53.0 / 255.0, green: 197.0 / 255.0, blue: 240.0 / 255.0, alpha: 1)
+    }
+
+    @nonobjc class var point: UIColor {
+        return UIColor(red: 255.0 / 255.0, green: 119.0 / 255.0, blue: 119.0 / 255.0, alpha: 1)
+    }
+
+    @nonobjc class var white: UIColor {
+        return UIColor(red: 255.0 / 255.0, green: 255.0 / 255.0, blue: 255.0 / 255.0, alpha: 1)
+    }
+    
+    @nonobjc class var black: UIColor {
+        return UIColor(red: 47.0 / 255.0, green: 52.0 / 255.0, blue: 56.0 / 255.0, alpha: 1)
+    }
+
+    @nonobjc class var darkGray: UIColor {
+        return UIColor(red: 130.0 / 255.0, green: 140.0 / 255.0, blue: 148.0 / 255.0, alpha: 1)
+    }
+
+    @nonobjc class var gray: UIColor {
+        return UIColor(red: 227.0 / 255.0, green: 229.0 / 255.0, blue: 229.0 / 255.0, alpha: 1)
+    }
+
+    @nonobjc class var lightGray: UIColor {
+        return UIColor(red: 239.0 / 255.0, green: 241.0 / 255.0, blue: 242.0 / 255.0, alpha: 1)
+    }
+
+    @nonobjc class var gray_20: UIColor {
+        return UIColor(red: 247.0 / 255.0, green: 249.0 / 255.0, blue: 250.0 / 255.0, alpha: 1)
+    }
+
+}
