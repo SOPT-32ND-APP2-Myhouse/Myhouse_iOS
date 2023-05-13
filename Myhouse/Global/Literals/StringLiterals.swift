3f83be73892543607ced723def2f42d534756b9a
Choi YoungLyn <joeuni0129@gmail.com>
2023-05-14 04:13:16 +0900

    [Setting] #5 - StringLiterals 추가

---
+++ Myhouse/Global/Literals/StringLiterals.swift
@@ -0,0 +1,16 @@
+//
+//  StringLiterals.swift
+//  Myhouse
+//
+//  Created by 최영린 on 2023/05/14.
+//
+
+import Foundation
+
+struct I18N {
+    
+    struct Home {
+        static let searchPlaceholder = "오늘의 집 통합검색"
+    }
+    
+}
