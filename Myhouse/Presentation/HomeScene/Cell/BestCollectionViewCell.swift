cbe44e6b1f57301f2960ccb7f3ba8fa2f7f73a39
frohsch <kminjoo1213@gmail.com>
2023-05-15 16:11:47 +0900

    [Feat] #10 - 홈뷰 이주의 인기 콘텐츠 셀 구현

---
+++ Myhouse/Presentation/HomeScene/Cell/BestCollectionViewCell.swift
@@ -0,0 +1,102 @@
+//
+//  CollectionViewCell.swift
+//  Myhouse
+//
+//  Created by Minjoo Kim on 2023/05/15.
+//
+
+import UIKit
+
+import SnapKit
+
+class BestContentsCollectionViewCell: UICollectionViewCell, UICollectionViewRegisterable {
+    
+    
+    static var isFromNib: Bool = false
+    
+    // MARK: - UI Components
+    
+    private let liveImageView = UIImageView()
+    private let rankLabel = UILabel()
+    private let channelLabel = UILabel()
+    private let titleLabel = UILabel()
+    private let ratingLabel = UILabel()
+    
+    // MARK: - Life Cycles
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setUI()
+        setLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+// MARK: - Extensions
+
+extension BestContentsCollectionViewCell {
+    private func setUI() {
+        liveImageView.do {
+            $0.layer.cornerRadius = 3
+        }
+        
+        rankLabel.do {
+            $0.font = .PretendardBold(size: 19)
+        }
+        
+        channelLabel.do {
+            $0.font = .PretendardRegular(size: 10)
+        }
+        
+        titleLabel.do {
+            $0.font = .PretendardRegular(size: 10)
+        }
+        
+        ratingLabel.do {
+            $0.font = .PretendardRegular(size: 10)
+        }
+    }
+    
+    private func setLayout() {
+        contentView.addSubviews(liveImageView, rankLabel, channelLabel, titleLabel, ratingLabel)
+        
+        liveImageView.snp.makeConstraints {
+            $0.top.equalToSuperview()
+            $0.height.equalTo(80)
+            $0.width.equalTo(160)
+            $0.centerX.equalToSuperview()
+        }
+        
+        rankLabel.snp.makeConstraints {
+            $0.top.equalTo(liveImageView.snp.bottom).offset(8)
+            $0.leading.equalToSuperview().inset(6)
+        }
+        
+        channelLabel.snp.makeConstraints {
+            $0.top.equalTo(liveImageView.snp.bottom).offset(11)
+            $0.leading.equalTo(rankLabel.snp.trailing).offset(5)
+        }
+        
+        titleLabel.snp.makeConstraints {
+            $0.top.equalTo(channelLabel.snp.bottom).offset(2)
+            $0.leading.equalTo(channelLabel.snp.leading)
+        }
+        
+        ratingLabel.snp.makeConstraints {
+            $0.top.equalTo(titleLabel.snp.bottom).offset(2)
+            $0.leading.equalTo(channelLabel.snp.leading)
+        }
+    }
+    
+//    func configureCell(_ livaData: LiveDataModel) {
+//        liveImageView.image = livaData.image
+//        rankLabel.text = "\(livaData.rank)"
+//        channelLabel.text = livaData.channel
+//        titleLabel.text = livaData.title
+//        ratingLabel.text = "\(livaData.rating)%"
+//    }
+}
