ebb33674c24959533d4c014ebb686cc6383ed382
frohsch <kminjoo1213@gmail.com>
2023-05-15 16:17:25 +0900

    [Feat] #10 - 홈뷰, 컬렉션뷰 추가

---
+++ Myhouse/Presentation/HomeScene/View/HomeView.swift
@@ -0,0 +1,151 @@
+//
+//  HomeView.swift
+//  Myhouse
+//
+//  Created by Minjoo Kim on 2023/05/15.
+//
+
+import UIKit
+
+import SnapKit
+
+final class HomeView: BaseView {
+    
+    private typealias SectionType = AboutSection
+    
+    @frozen
+    private enum AboutSection: CaseIterable {
+        case best
+    }
+    
+    // MARK: - UI Components
+    
+    private let imageDummy = HomeDataModel.dummy()
+    
+    private lazy var homeCollectionView: UICollectionView = {
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: self.getLayout())
+        collectionView.contentInsetAdjustmentBehavior = .never
+        collectionView.collectionViewLayout = self.getLayout()
+        collectionView.showsVerticalScrollIndicator = false
+        return collectionView
+    }()
+    
+    // MARK: - Life Cycles
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        registerCell()
+        setDataSource()
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    func setDataSource() {
+        homeCollectionView.dataSource = self
+    }
+    
+    override func setLayout() {
+        self.addSubviews(homeCollectionView)
+        
+        homeCollectionView.snp.makeConstraints {
+            $0.leading.trailing.bottom.equalToSuperview()
+            $0.top.equalToSuperview()
+        }
+    }
+}
+
+// MARK: - Extensions
+
+extension HomeView {
+    func registerCell() {
+        BestCollectionViewCell.register(target: homeCollectionView)
+        CollectionReusableView.register(target: homeCollectionView)
+    }
+    
+    func getLayout() -> UICollectionViewLayout {
+        return UICollectionViewCompositionalLayout { sectionIndex, env -> NSCollectionLayoutSection? in
+            let sectionType = SectionType.allCases[sectionIndex]
+            switch sectionType {
+            case .best:
+                return self.getLayoutBestSection()
+            }
+        }
+    }
+    
+
+    func getLayoutBestSection() -> NSCollectionLayoutSection {
+        let itemSize = NSCollectionLayoutSize(
+            widthDimension: .fractionalWidth(1),
+            heightDimension: .fractionalHeight(1.0)
+        )
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        item.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0)
+        
+        let groupSize = NSCollectionLayoutSize(
+            widthDimension: .absolute(150),
+            heightDimension: .absolute(205)
+        )
+        let group = NSCollectionLayoutGroup.horizontal(
+            layoutSize: groupSize,
+            subitems: [item]
+        )
+        group.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 5, bottom: 0, trailing: 5)
+        let headerSize = NSCollectionLayoutSize(
+            widthDimension: .fractionalWidth(1.0),
+            heightDimension: .absolute(40)
+        )
+        
+        let header = NSCollectionLayoutBoundarySupplementaryItem(
+            layoutSize: headerSize,
+            elementKind: UICollectionView.elementKindSectionHeader,
+            alignment: .top
+        )
+        
+        let section = NSCollectionLayoutSection(group: group)
+        section.orthogonalScrollingBehavior = .continuous
+        section.boundarySupplementaryItems = [header]
+        section.contentInsets = .init(top: 0, leading: 10, bottom: 0, trailing: 10)
+        return section
+    }
+    
+}
+
+extension HomeView: UICollectionViewDataSource {
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return AboutSection.allCases.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        let sectionType = SectionType.allCases[section]
+        switch sectionType {
+        case .best:
+            return imageDummy.count
+        }
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let sectionType = SectionType.allCases[indexPath.section]
+        switch sectionType {
+        case .best:
+            let cell = BestCollectionViewCell.dequeueReusableCell(collectionView: collectionView, indexPath: indexPath)
+            cell.configureCell(imageDummy[indexPath.item])
+            return cell
+            
+        }
+    }
+    
+    
+    func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        
+        let sectionType = SectionType.allCases[indexPath.section]
+        switch sectionType {
+        case .best:
+            let headerView = CollectionReusableView.dequeueReusableHeaderView(collectionView: collectionView, indexPath: indexPath)
+            headerView.setSectionTitle(text: I18N.HomeSection.best)
+            return headerView
+        }
+    }
+}
