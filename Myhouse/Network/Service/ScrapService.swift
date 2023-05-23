a8922b1b67e875e653fdd363bc55cf0e8c7280f3
Choi YoungLyn <joeuni0129@gmail.com>
2023-05-23 23:44:13 +0900

    [Add] #32 - 네트워크 파일 폴더링 및 파일 추가

---
+++ Myhouse/Network/Service/ScrapService.swift
@@ -0,0 +1,21 @@
+//
+//  ScrapService.swift
+//  Myhouse
+//
+//  Created by 최영린 on 2023/05/23.
+//
+
+import Foundation
+
+import Alamofire
+
+final class ScrapService: BaseService {
+    
+    static let shared = ScrapService()
+    
+    private override init() {}
+}
+
+extension ScrapService {
+    
+}
