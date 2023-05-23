7a890fe1ff729fd1cb1c1e0fa69b1d3cb7b28eb3
Choi YoungLyn <joeuni0129@gmail.com>
2023-05-23 23:43:49 +0900

    [Add] #32 - Network Base 파일 추가

---
+++ Myhouse/Network/Base/NetworkConstant.swift
@@ -0,0 +1,14 @@
+//
+//  NetworkConstant.swift
+//  Myhouse
+//
+//  Created by 최영린 on 2023/05/23.
+//
+
+import Foundation
+
+import Alamofire
+
+struct NetworkConstant {
+    static let noTokenHeader: HTTPHeaders = ["Content-Type": "application/json"]
+}
