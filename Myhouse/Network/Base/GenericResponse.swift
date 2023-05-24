a29f4ab82cf1436df46cdd0edec574b380f5a60c
Choi YoungLyn <joeuni0129@gmail.com>
2023-05-24 10:28:27 +0900

    [Chore] #32 - Generic Response 수정

--- Myhouse/Network/Base/GenericResponse.swift
+++ Myhouse/Network/Base/GenericResponse.swift
@@ -14,13 +14,6 @@ struct GenericResponse<T: Codable>: Codable {
     var message: String
     var data: T?
     
-    enum CodingKeys: String, CodingKey {
-        case status = "status"
-        case success = "success"
-        case message = "message"
-        case data = "data"
-    }
-    
     init(from decoder: Decoder) throws {
         let values = try decoder.container(keyedBy: CodingKeys.self)
         status = (try? values.decode(Int.self, forKey: .status)) ?? -1
